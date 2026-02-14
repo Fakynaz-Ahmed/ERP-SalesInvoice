e52ccac91d1b35cb0a746f2b89c0c22fd301b32d
Fakynaz-Ahmed <fakyahmed4@gmail.com>
2026-02-14 04:37:45 +0200

    add ERPDbScript.sql

---
+++ ERPDbScript.sql
@@ -0,0 +1,214 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Customers] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [Phone] nvarchar(max) NOT NULL,
+        [Address] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Items] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Price] decimal(18,2) NOT NULL,
+        [Quantity] int NOT NULL,
+        CONSTRAINT [PK_Items] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Invoices] (
+        [Id] int NOT NULL IDENTITY,
+        [InvoiceNumber] nvarchar(max) NOT NULL,
+        [InvoiceDate] datetime2 NOT NULL,
+        [CustomerId] int NOT NULL,
+        [TotalAmount] decimal(18,2) NOT NULL,
+        CONSTRAINT [PK_Invoices] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Invoices_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [Customers] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [InvoiceDetails] (
+        [Id] int NOT NULL IDENTITY,
+        [InvoiceId] int NOT NULL,
+        [ItemId] int NOT NULL,
+        [Quantity] int NOT NULL,
+        [Price] decimal(18,2) NOT NULL,
+        CONSTRAINT [PK_InvoiceDetails] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_InvoiceDetails_Invoices_InvoiceId] FOREIGN KEY ([InvoiceId]) REFERENCES [Invoices] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_InvoiceDetails_Items_ItemId] FOREIGN KEY ([ItemId]) REFERENCES [Items] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_InvoiceDetails_InvoiceId] ON [InvoiceDetails] ([InvoiceId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_InvoiceDetails_ItemId] ON [InvoiceDetails] ([ItemId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Invoices_CustomerId] ON [Invoices] ([CustomerId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213193927_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260213193927_InitialCreate', N'8.0.24');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213205933_SeedData'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Address', N'Email', N'Name', N'Phone') AND [object_id] = OBJECT_ID(N'[Customers]'))
+        SET IDENTITY_INSERT [Customers] ON;
+    EXEC(N'INSERT INTO [Customers] ([Id], [Address], [Email], [Name], [Phone])
+    VALUES (1, N''Cairo'', N''ahmed@gmail.com'', N''Ahmed Ali'', N''0100000001''),
+    (2, N''Giza'', N''sara@gmail.com'', N''Sara Mohamed'', N''0100000002''),
+    (3, N''Qena'', N''omar@gmail.com'', N''Omar Hassan'', N''0100000003'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Address', N'Email', N'Name', N'Phone') AND [object_id] = OBJECT_ID(N'[Customers]'))
+        SET IDENTITY_INSERT [Customers] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213205933_SeedData'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Name', N'Price', N'Quantity') AND [object_id] = OBJECT_ID(N'[Items]'))
+        SET IDENTITY_INSERT [Items] ON;
+    EXEC(N'INSERT INTO [Items] ([Id], [Name], [Price], [Quantity])
+    VALUES (1, N''Laptop'', 15000.0, 10),
+    (2, N''Mouse'', 150.0, 50),
+    (3, N''Keyboard'', 400.0, 30),
+    (4, N''Monitor'', 3500.0, 20)');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Name', N'Price', N'Quantity') AND [object_id] = OBJECT_ID(N'[Items]'))
+        SET IDENTITY_INSERT [Items] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260213205933_SeedData'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260213205933_SeedData', N'8.0.24');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260214022552_EditInModels'
+)
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Items]') AND [c].[name] = N'Name');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Items] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Items] ALTER COLUMN [Name] nvarchar(100) NOT NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260214022552_EditInModels'
+)
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Invoices]') AND [c].[name] = N'InvoiceNumber');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Invoices] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Invoices] ALTER COLUMN [InvoiceNumber] nvarchar(50) NOT NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260214022552_EditInModels'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260214022552_EditInModels', N'8.0.24');
+END;
+GO
+
+COMMIT;
+GO
+
